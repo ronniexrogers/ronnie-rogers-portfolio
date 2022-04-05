201d3437cf4c2f369e5ceb356fbd90113bfcaebb
ronniexrogers <ronniexrogers@yahoo.com>
2022-04-05 09:14:19 -0700

    working on nav

--- src/app_components/Nav.js
+++ src/app_components/Nav.js
@@ -1,7 +1,35 @@
+import { NavItem, NavLink } from "reactstrap"
+
 const Nav = () => {
     return ( 
         <div className="nav">
-            yo, i'm the nav component
+            <Nav justified tabs>
+                <NavItem>
+                    <NavLink active href="/">
+                        Home
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/About">
+                        About
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/Contact">
+                        Contact
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/Portfolio">
+                        Portfolio
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/Resume">
+                        Resume
+                    </NavLink>
+                </NavItem>
+            </Nav>
         </div>
      )
 }
