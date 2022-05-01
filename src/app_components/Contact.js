7714dd35a81e0289b22e9a546f5665a6f0376d94
ronniexrogers <ronniexrogers@yahoo.com>
2022-05-01 14:18:15 -0700

    working on contact form

--- src/app_components/Contact.js
+++ src/app_components/Contact.js
@@ -1,17 +1,45 @@
+import { useState } from "react"
+const axios = require('axios')
+
 const Contact = () => {
+
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+    const [email, setEmail] = useState('')
+    const formSparkUrl = `https://submit-form.com/ageibyG6`
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await postSubmission()
+    }
+
+    const postSubmission = async () => {
+        const formData = {
+            name: name,
+            email: email,
+            message: message,
+        }
+        try {
+            await axios.post(formSparkUrl, formData)
+        }catch(err) {
+            alert(err)
+        }
+    }
+
     return ( 
         <div className="contact">
-            <form action="https://submit-form.com/ageibyG6">
+            <form onSubmit={ handleSubmit } action="https://submit-form.com/ageibyG6">
                 <label for="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="Name" required="true" />
+                <input type="text" id="name" name="name" placeholder="Name" required="true" onChange={(e) => setName(e.target.value)}  />
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Email" required="true" />
+                <input type="email" id="email" name="email" placeholder="Email" required="true" onChange={(e) => setEmail(e.target.value)}/>
                 <label for="message">Message</label>
                 <textarea
                     id="message"
                     name="message"
                     placeholder="Message"
                     required="true"
+                    onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
                 <button type="submit">Send</button>
             </form>
