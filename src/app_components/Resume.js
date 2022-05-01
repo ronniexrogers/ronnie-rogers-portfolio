0fd3863f8a26447902af613f59ba496f501753f6
ronniexrogers <ronniexrogers@yahoo.com>
2022-05-01 15:02:17 -0700

    working on file download

--- src/app_components/Resume.js
+++ src/app_components/Resume.js
@@ -1,7 +1,7 @@
 const Resume = () => {
     return ( 
         <div className="resume">
-            <a href="../../Resources/Ronnie-Rogers-Resume.pdf" download="proposed_file_name">Download Resume</a>
+            <a download href="./Resources/Ronnie-Rogers-Resume.pdf">Download Resume</a>
         </div>
      )
 }
