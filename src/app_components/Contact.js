c352f7557d33265edeef18830c89d6d38321cfe3
ronniexrogers <ronniexrogers@yahoo.com>
2022-05-01 14:03:22 -0700

    fixing deployment issue

--- src/app_components/Contact.js
+++ src/app_components/Contact.js
@@ -1,7 +1,20 @@
 const Contact = () => {
     return ( 
         <div className="contact">
-            yo, i'm the contact component
+            <form action="https://submit-form.com/ageibyG6">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Name" required="true" />
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" required="true" />
+                <label for="message">Message</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    placeholder="Message"
+                    required="true"
+                ></textarea>
+                <button type="submit">Send</button>
+            </form>
         </div>
      )
 }
