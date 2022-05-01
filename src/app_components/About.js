bee27bbdd4cdfc18241bc87394912cefb0895ebe
ronniexrogers <ronniexrogers@yahoo.com>
2022-05-01 13:38:11 -0700

    working on about page

--- src/app_components/About.js
+++ src/app_components/About.js
@@ -1,7 +1,22 @@
 const About = () => {
     return ( 
         <div className="about">
-            yo, i'm the about component
+            <h3>Who am I?</h3>
+            <div className="about-text">
+                My name is Ronnie Raymond Rogers. (Yes, my initials are RRR.) I'm a 28 year-old software engineer currently living in the Greater Los Angeles area.  I began my tech journey after spending the first 9 years of my professional career as an HVAC/R technician.  I began self-teaching web development by watching videos and completing courses on Free Code Camp.  After about six months of self-teaching, I enrolled in the Software Engineering Immersive course with General Assembly.  I graduated as of March 2022 and am currently seeking my first professional role as a developer.  
+            </div>
+            <br></br>
+            <h3>What am I in to?</h3>
+            <div className="about-text">
+                Aside from web development, some of my passions and interests include:
+                <ul className="list">
+                    <li>-Going to concerts</li>
+                    <li>-Fashion design</li>
+                    <li>-Gaming</li>
+                    <li>-Hiking</li>
+                    <li>-Traveling</li>
+                </ul>
+            </div>
         </div>
      )
 }
