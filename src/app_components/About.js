afcd6ba0a092ab26cc7d6e531b7c6dadc57ce012
ronniexrogers <ronniexrogers@yahoo.com>
2022-05-02 16:49:15 -0700

    more content addition

--- src/app_components/About.js
+++ src/app_components/About.js
@@ -1,3 +1,5 @@
+import { List } from "reactstrap"
+
 const About = () => {
     return ( 
         <div className="about">
@@ -9,14 +11,22 @@ const About = () => {
             <h3>What am I in to?</h3>
             <div className="about-text">
                 Aside from web development, some of my passions and interests include:
+                <List type="unstyled">
                 <ul className="list">
-                    <li>-Going to concerts</li>
-                    <li>-Fashion design</li>
-                    <li>-Gaming</li>
-                    <li>-Hiking</li>
-                    <li>-Traveling</li>
+                    <li>Going to concerts</li>
+                    <li>Fashion design</li>
+                    <li>Gaming</li>
+                    <li>Hiking</li>
+                    <li>Traveling</li>
                 </ul>
+                </List>
             </div>
+            <br></br>
+            <h3>What are my professional goals?</h3>
+            <div className="about-text">
+                My goal is to land a position in a field that I am passionate about and already knowledgable in.  I would love to work in a role as a developer for a fashion company, gaming company, or HVAC/R company.  I feel it is much more fulfilling to work on projects where you are passionate about the product.
+            </div>
+            <br></br>
         </div>
      )
 }
